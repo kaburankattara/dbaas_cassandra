67ca6cb3491204f1d6f4da0e30de5c43dc9ba3f0
平井宏 <h.hirai.minori@gmail.com>
2020-07-16 08:22:16 +0900

    メッセージ表示の共通処理実装＋ログイン画面の認証エラー処理追加

---
+++ src/main/java/com/dbaas/cassandra/consts/SysConsts.java
@@ -0,0 +1,12 @@
+package com.dbaas.cassandra.consts;
+
+/**
+ * システム的観点の定数クラス
+ */
+public class SysConsts {
+	
+	public static final String MESSAGE_KEY_INFO = "message";
+	
+	public static final String MESSAGE_KEY_ERROR = "errorMessage";
+	
+}
