8da80a1ab08efac6239e01d442f52a8aeb35847a
hiroshi <>
2021-04-24 23:03:11 +0900

    エンドポイントのコピー機能を実装

---
+++ src/main/resources/static/js/common/clipBoardCopy.js
@@ -0,0 +1,25 @@
+var ClipBoardCopy = /** @class */ (function () {
+    function ClipBoardCopy() {
+        this.execById = function (tagId) {
+            // コピーする文章の取得
+            var text = $("#" + tagId).text();
+            // テキストエリアの作成
+            var $textarea = $('<textarea></textarea>');
+            // テキストエリアに文章を挿入
+            $textarea.text(text);
+            //　テキストエリアを挿入
+            $("#form").append($textarea);
+            //　テキストエリアを選択
+            $textarea.select();
+            // コピー
+            document.execCommand('copy');
+            // テキストエリアの削除
+            $textarea.remove();
+            // アラート文の表示
+            alert("クリップボードにコピーしました。");
+            //$('#js-copyalert').show().delay(2000).fadeOut(400);
+        };
+    }
+    return ClipBoardCopy;
+}());
+export { ClipBoardCopy };
